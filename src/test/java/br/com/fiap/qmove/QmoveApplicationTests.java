6f278334f5cc89444626d8b1e01684e9e537d9b7
hmarinho <rm558841@fiap.com.br>
2025-05-03 21:57:34 -0300

    Primeiro Commit

---
+++ src/test/java/br/com/fiap/qmove/QmoveApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.qmove;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QmoveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
